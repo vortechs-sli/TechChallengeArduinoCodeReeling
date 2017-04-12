0f38424d34c4dd6d8954112fad6372483daa7a28
vortechs-sli <vortechs.sli@gmail.com>
2017-04-11 19:12:50 -0700

    Add files via upload

---
+++ TechChallengeArduinoCodeReeling.ino
@@ -0,0 +1,109 @@
+#define E1 5  // Enable Pin for motor 1
+#define E2 4  // Enable Pin for motor 2
+
+#define I1 3  // Control pin 1 for motor 1
+#define I2 2  // Control pin 2 for motor 1
+#define I3 1  // Control pin 1 for motor 2
+#define I4 0  // Control pin 2 for motor 2
+
+void setup() {
+
+    pinMode(E1, OUTPUT);
+    pinMode(E2, OUTPUT);
+//
+    pinMode(I1, OUTPUT);
+    pinMode(I2, OUTPUT);
+    pinMode(I3, OUTPUT);
+    pinMode(I4, OUTPUT);
+//
+    digitalWrite(E1, HIGH);
+    digitalWrite(E2, HIGH);
+//
+    digitalWrite(I1, HIGH);
+    digitalWrite(I2, LOW);
+    digitalWrite(I3, HIGH);
+    digitalWrite(I4, LOW);
+
+}
+
+void loop() {
+
+    // off ten seconds
+    digitalWrite(E1, LOW);
+    digitalWrite(E2, LOW);
+    delay(10000);
+
+    // drive forward
+    // off ten seconds
+    digitalWrite(E1, LOW);
+    digitalWrite(E2, LOW);
+    delay(10000);
+
+    // bridge to be lowered
+    digitalWrite(E1, HIGH);
+    digitalWrite(E2, HIGH);
+    digitalWrite(I1, HIGH);
+    digitalWrite(I2, LOW);
+    digitalWrite(I3, HIGH);
+    digitalWrite(I4, LOW);
+    delay(10000);
+
+    // drive forward
+    // off ten seconds
+    digitalWrite(E1, LOW);
+    digitalWrite(E2, LOW);
+    delay(10000);
+    
+    // bridge to be raised
+    digitalWrite(E1, HIGH);
+    digitalWrite(E2, HIGH);
+    digitalWrite(I1, LOW);
+    digitalWrite(I2, HIGH);
+    digitalWrite(I3, LOW);
+    digitalWrite(I4, HIGH);
+    delay(10000);
+
+    // drive forward
+    // off ten seconds
+    digitalWrite(E1, LOW);
+    digitalWrite(E2, LOW);
+    delay(10000);
+    
+    // turn 180
+    // off ten seconds
+    digitalWrite(E1, LOW);
+    digitalWrite(E2, LOW);
+    delay(10000);
+
+    // drive backward
+    // off ten seconds
+    digitalWrite(E1, LOW);
+    digitalWrite(E2, LOW);
+    delay(10000);
+
+    // bridge to be lowered
+    digitalWrite(E1, HIGH);
+    digitalWrite(E2, HIGH);
+    digitalWrite(I1, LOW);
+    digitalWrite(I2, HIGH);
+    digitalWrite(I3, LOW);
+    digitalWrite(I4, HIGH);
+    delay(10000);
+
+    // drive backward
+    // off ten seconds
+    digitalWrite(E1, LOW);
+    digitalWrite(E2, LOW);
+    delay(10000);
+
+    // off ten seconds
+    // time for bridge to be raised
+    digitalWrite(E1, HIGH);
+    digitalWrite(E2, HIGH);
+    digitalWrite(I1, HIGH);
+    digitalWrite(I2, LOW);
+    digitalWrite(I3, HIGH);
+    digitalWrite(I4, LOW);
+    delay(10000);
+}
+
